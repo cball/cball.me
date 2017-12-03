460013516ddc15c66dd7d113bdfdd348d16cbefa
Chris Ball <chris@echobind.com>
2017-12-03 15:02:29 -0500

    add more redirects, delete unused page, add site title

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,54 @@
 'use strict';
 
+// Use this file for redirects instead of Netlify's _redirects.
+// This keeps the configuration the same regardless of host.
 exports.createPages = ({ boundActionCreators }) => {
   const { createRedirect } = boundActionCreators;
 
   createRedirect({
     fromPath: `/bring-some-es6-awesome-with-esnext`,
+    toPath: `/archive/old-ember-post/`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
+
+  createRedirect({
+    fromPath: `/organize-your-ember-app-with-pods`,
+    toPath: `/archive/old-ember-post/`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
+
+  createRedirect({
+    fromPath: `/i18n-in-practice`,
+    toPath: `/archive/old-ember-post/`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
+
+  createRedirect({
+    fromPath: `/not-using-i18n-you-probably-should-be`,
+    toPath: `/archive/old-ember-post/`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
+
+  createRedirect({
+    fromPath: `/understanding-loading-substates-in-ember`,
+    toPath: `/archive/old-ember-post/`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
+
+  createRedirect({
+    fromPath: `/non-restful-api-calls-with-ember-data`,
+    toPath: `/archive/old-ember-post/`,
+    isPermanent: true,
+    redirectInBrowser: true
+  });
+
+  createRedirect({
+    fromPath: `/help-i-have-no-tests`,
     toPath: `/archive/old-ember-post/`,
     isPermanent: true,
     redirectInBrowser: true
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,13 +7,13 @@ import './index.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    {/* <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' }
-      ]}
-    /> */}
+    <Helmet
+      title="Chris Ball - Speaker, Maker, Entreprenuer."
+      // meta={[
+      //   { name: 'description', content: '' },
+      //   { name: 'keywords', content: 'sample, something' }
+      // ]}
+    />
     <div className="container">
       <Sidebar />
       <main>
